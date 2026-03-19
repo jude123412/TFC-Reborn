7697ce694026473a62d5c589ee1f4e27a3be8922
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-03-19 09:43:08 +0000

    add recipes for forge microblocks

---
+++ groovy/postInit/init/forge_microblocks/recipes.groovy
@@ -0,0 +1,34 @@
+crafting.remove('microblockcbe:stone_rod')
+crafting.remove('microblockcbe:stone_saw')
+crafting.remove('microblockcbe:iron_saw')
+crafting.remove('microblockcbe:diamond_saw')
+
+// Stone Saw/Copper Block Saw
+crafting.shapedBuilder()
+    .output(item('microblockcbe:saw_stone'))
+    .shape([
+        [null,null,null],
+        [ore('stickSilver'),ore('stickSilver'),ore('stickWood')],
+        [ore('stickSilver'),item('tfc:metal/saw_blade/copper'),ore('stickWood')]
+    ])
+    .register()
+
+// Iron Saw/Wrought Iron Block Saw
+crafting.shapedBuilder()
+    .output(item('microblockcbe:saw_iron'))
+    .shape([
+        [null,null,null],
+        [ore('stickSilver'),ore('stickSilver'),ore('stickWood')],
+        [ore('stickSilver'),item('tfc:metal/saw_blade/bismuth_bronze'),ore('stickWood')]
+    ])
+    .register()
+
+// Diamond Saw/Black Steel Block Saw
+crafting.shapedBuilder()
+    .output(item('microblockcbe:saw_diamond'))
+    .shape([
+        [null,null,null],
+        [ore('stickSilver'),ore('stickSilver'),ore('stickWood')],
+        [ore('stickSilver'),item('tfc:metal/saw_blade/wrought_iron'),ore('stickWood')]
+    ])
+    .register()
