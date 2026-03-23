b4a1459aa37d4f691f941c8dbbb846ae93ba1cf4
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-03-23 19:00:59 +0000

    Chisels & Bits Recipes + Config

---
+++ groovy/postInit/init/chisel_bits/recipes.groovy
@@ -0,0 +1,63 @@
+def recipesToRemove = [
+    'chiselsandbits:positiveprint',
+    'chiselsandbits:negativeprint',
+    'chiselsandbits:mirrorprint',
+    'chiselsandbits:bit_bag',
+]
+// Positive Chisel Design
+crafting.shapelessBuilder()
+    .output(item('chiselsandbits:positiveprint'))
+    .input([
+         ore('paper'),
+         ore('dyeBlue')
+    ])
+    .register()
+
+// Negative Chisel Design
+crafting.shapelessBuilder()
+    .output(item('chiselsandbits:negativeprint'))
+    .input([
+         ore('paper'),
+         ore('dyeRed')
+    ])
+    .register()
+
+// Mirror Chisel Design
+crafting.shapelessBuilder()
+    .output(item('chiselsandbits:mirrorprint'))
+    .input([
+         ore('paper'),
+         ore('dyeYellow')
+    ])
+    .register()
+
+//Chiseled Bit Bag
+crafting.shapedBuilder()
+    .output(item('chiselsandbits:bit_bag'))
+    .shape([
+        [null,ore('string'),null],
+        [null,item('tfc:hide/raw/small'),null],
+        [null,null,null]
+    ])
+    .register()
+
+// Recipe Removal
+for (var in recipesToRemove) {
+    crafting.remove(var)
+}
+
+// crafting.shapedBuilder()
+//     .output(null)
+//     .shape([
+//         [null,null,null],
+//         [null,null,null],
+//         [null,null,null]
+//     ])
+//     .register()
+
+// crafting.shapelessBuilder()
+//     .output(null)
+//     .input([
+//          null
+//     ])
+//     .register()
