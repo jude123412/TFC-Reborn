62951b4f5c2a0f3745f79fe25ade57e7d2485389
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-09-25 20:23:42 +0100

    Finish unification for plates

--- kubejs/server_scripts/mods/unification.js
+++ kubejs/server_scripts/mods/unification.js
@@ -1,14 +1,49 @@
 TFCEvents.data(event => {
+    // Plate Loop
     for (const metal in global.metals) {
         let m = global.metals[metal]
         event.itemHeat(`kubejs:metal/plate/${metal}`, m.capacity, m.work, m.weld)
     }
 })
 
-ServerEvents.recipes(event => {
-    const tfc = event.recipes.tfc
+ServerEvents.tags('item', event => {
+    // Fix cast iron tag
+    event.remove('forge:ingots/cast_iron', 'minecraft:iron_ingot')
+    event.add('forge:ingots/wrought_iron', 'minecraft:iron_ingot')
+    
+    // Plate Loop
     for (const metal in global.metals) {
         let m = global.metals[metal]
+
+        if (m.extra_tags) {
+            for (const t of m.extra_tags)
+            event.add(`forge:plates/${t}`, `kubejs:metal/plate/${metal}`)
+        }
+    }
+})
+
+ServerEvents.recipes(event => {
+    const tfc = event.recipes.tfc
+    const create = event.recipes.create
+    const ie = event.recipes.immersiveengineering
+
+    // Main plate Loop
+    for (const metal in global.metals) {
+        let m = global.metals[metal]
+
+        // Remove ALL recipes that output any duplicate plate
+        let plates = Ingredient.of(`#forge:plates/${metal}`).getItemIds();
+        for (let id of plates) {
+            event.remove({output: id});
+        }
+        if (m.extra_tags) {
+            for (let t of m.extra_tags) {
+                let extraPlates = Ingredient.of(`#forge:plates/${t}`).getItemIds();
+                for (let id of extraPlates) {
+                    event.remove({output: id});
+                }
+            }
+        }
 
         tfc.anvil(
             TFC.itemStackProvider.of(`kubejs:metal/plate/${metal}`).copyHeat(),
@@ -19,19 +54,16 @@ ServerEvents.recipes(event => {
             'hit_last'
         ]
         ).tier(m.tier)
+        create.pressing(`kubejs:metal/plate/${metal}`, `#forge:ingots/${metal}`)
+        ie.metal_press(
+            `kubejs:metal/plate/${metal}`,
+            `#forge:ingots/${metal}`,
+            'immersiveengineering:mold_plate',
+            1600
+        )
 
         tfc.heating(`kubejs:metal/plate/${metal}`, m.melt)
             .resultFluid(Fluid.of(m.fluid, 100))
     }
 })
 
-ServerEvents.tags('item', event => {
-    for (const metal in global.metals) {
-        let m = global.metals[metal]
-
-        if (m.extra_tags) {
-            for (const t of m.extra_tags)
-            event.add(`forge:plates/${t}`, `kubejs:metal/plate/${metal}`)
-        }
-    }
-})
