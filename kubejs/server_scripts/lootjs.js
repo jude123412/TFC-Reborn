84ad92068b59a710a6deb61d22391914dfbe3a8d
jude123412 <104434396+jude123412@users.noreply.github.com>
2026-09-26 13:47:04 +0100

    replace lootable iron and gold ingots with tfc variants

--- kubejs/server_scripts/lootjs.js
+++ kubejs/server_scripts/lootjs.js
@@ -2,4 +2,6 @@ LootJS.modifiers((event) => {
     // Matches any loot table that contains "chests" in its path (vanilla and modded dungeons)
     event.addLootTableModifier(/.*:chests\/.*/)
         .replaceLoot('tfc:metal/double_ingot/wrought_iron', 'tfc:metal/double_ingot/cast_iron', true)
+        .replaceLoot('minecraft:iron_ingot', 'tfc:metal/ingot/cast_iron', true)
+        .replaceLoot('minecraft:gold_ingot', 'tfc:metal/ingot/gold', true)
 });
